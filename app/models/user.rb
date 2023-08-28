a3f1b82b5586ffc38e8aaa6249b5eb745f5dc7bd
Hemershon Silva <hemershon@gmail.com>
2023-08-28 16:27:41 -0300

    add active and inactive for user (#5)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  scope :active, -> { where(active: true) }
+  scope :inactive, -> { where(active: false) }
+  
 end
